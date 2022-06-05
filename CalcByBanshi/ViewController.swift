932a4937b990390018b28966a0a430a8d8615b1c
author <wifi.oleg.london@gmail.com>
2022-06-05 16:55:05 +0300

    Initial Commit

---
+++ CalcByBanshi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalcByBanshi
+//
+//  Created by a2 on 05.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
